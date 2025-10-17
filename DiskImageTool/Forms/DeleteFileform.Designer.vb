45038a024aea107ce3a613db335fd15224501de1
Digitoxin1 <digitoxin@gmail.com>
2025-10-17 14:17:25 -0400

    Delete file dialog now resizes properly when changing the language to something other than English

--- DiskImageTool/Forms/DeleteFileform.Designer.vb
+++ DiskImageTool/Forms/DeleteFileform.Designer.vb
@@ -23,16 +23,20 @@ Partial Class DeleteFileForm
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(DeleteFileForm))
+        Dim GroupBox1 As System.Windows.Forms.GroupBox
+        Dim FlowLayoutPanel1 As System.Windows.Forms.FlowLayoutPanel
         Me.LblCaption = New System.Windows.Forms.Label()
-        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-        Me.RadioFillKeep = New System.Windows.Forms.RadioButton()
-        Me.RadioFill00 = New System.Windows.Forms.RadioButton()
-        Me.RadioFillF6 = New System.Windows.Forms.RadioButton()
         Me.Panel1 = New System.Windows.Forms.Panel()
         Me.BtnOK = New System.Windows.Forms.Button()
         Me.BtnCancel = New System.Windows.Forms.Button()
-        Me.GroupBox1.SuspendLayout()
+        Me.RadioFillKeep = New System.Windows.Forms.RadioButton()
+        Me.RadioFillF6 = New System.Windows.Forms.RadioButton()
+        Me.RadioFill00 = New System.Windows.Forms.RadioButton()
+        GroupBox1 = New System.Windows.Forms.GroupBox()
+        FlowLayoutPanel1 = New System.Windows.Forms.FlowLayoutPanel()
         Me.Panel1.SuspendLayout()
+        GroupBox1.SuspendLayout()
+        FlowLayoutPanel1.SuspendLayout()
         Me.SuspendLayout()
         '
         'LblCaption
@@ -40,37 +44,6 @@ Partial Class DeleteFileForm
         resources.ApplyResources(Me.LblCaption, "LblCaption")
         Me.LblCaption.Name = "LblCaption"
         Me.LblCaption.UseMnemonic = False
-        '
-        'GroupBox1
-        '
-        resources.ApplyResources(Me.GroupBox1, "GroupBox1")
-        Me.GroupBox1.Controls.Add(Me.RadioFillKeep)
-        Me.GroupBox1.Controls.Add(Me.RadioFill00)
-        Me.GroupBox1.Controls.Add(Me.RadioFillF6)
-        Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.TabStop = False
-        '
-        'RadioFillKeep
-        '
-        resources.ApplyResources(Me.RadioFillKeep, "RadioFillKeep")
-        Me.RadioFillKeep.Checked = True
-        Me.RadioFillKeep.Name = "RadioFillKeep"
-        Me.RadioFillKeep.TabStop = True
-        Me.RadioFillKeep.UseVisualStyleBackColor = True
-        '
-        'RadioFill00
-        '
-        resources.ApplyResources(Me.RadioFill00, "RadioFill00")
-        Me.RadioFill00.Name = "RadioFill00"
-        Me.RadioFill00.UseMnemonic = False
-        Me.RadioFill00.UseVisualStyleBackColor = True
-        '
-        'RadioFillF6
-        '
-        resources.ApplyResources(Me.RadioFillF6, "RadioFillF6")
-        Me.RadioFillF6.Name = "RadioFillF6"
-        Me.RadioFillF6.UseMnemonic = False
-        Me.RadioFillF6.UseVisualStyleBackColor = True
         '
         'Panel1
         '
@@ -94,14 +67,51 @@ Partial Class DeleteFileForm
         Me.BtnCancel.Name = "BtnCancel"
         Me.BtnCancel.UseVisualStyleBackColor = True
         '
+        'GroupBox1
+        '
+        resources.ApplyResources(GroupBox1, "GroupBox1")
+        GroupBox1.Controls.Add(FlowLayoutPanel1)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.TabStop = False
+        '
+        'FlowLayoutPanel1
+        '
+        resources.ApplyResources(FlowLayoutPanel1, "FlowLayoutPanel1")
+        FlowLayoutPanel1.Controls.Add(Me.RadioFillKeep)
+        FlowLayoutPanel1.Controls.Add(Me.RadioFillF6)
+        FlowLayoutPanel1.Controls.Add(Me.RadioFill00)
+        FlowLayoutPanel1.Name = "FlowLayoutPanel1"
+        '
+        'RadioFillKeep
+        '
+        resources.ApplyResources(Me.RadioFillKeep, "RadioFillKeep")
+        Me.RadioFillKeep.Checked = True
+        Me.RadioFillKeep.Name = "RadioFillKeep"
+        Me.RadioFillKeep.TabStop = True
+        Me.RadioFillKeep.UseVisualStyleBackColor = True
+        '
+        'RadioFillF6
+        '
+        resources.ApplyResources(Me.RadioFillF6, "RadioFillF6")
+        Me.RadioFillF6.Name = "RadioFillF6"
+        Me.RadioFillF6.UseMnemonic = False
+        Me.RadioFillF6.UseVisualStyleBackColor = True
+        '
+        'RadioFill00
+        '
+        resources.ApplyResources(Me.RadioFill00, "RadioFill00")
+        Me.RadioFill00.Name = "RadioFill00"
+        Me.RadioFill00.UseMnemonic = False
+        Me.RadioFill00.UseVisualStyleBackColor = True
+        '
         'DeleteFileForm
         '
         resources.ApplyResources(Me, "$this")
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.Window
         Me.CancelButton = Me.BtnCancel
+        Me.Controls.Add(GroupBox1)
         Me.Controls.Add(Me.Panel1)
-        Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.LblCaption)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
         Me.MaximizeBox = False
@@ -109,19 +119,20 @@ Partial Class DeleteFileForm
         Me.Name = "DeleteFileForm"
         Me.ShowIcon = False
         Me.ShowInTaskbar = False
-        Me.GroupBox1.ResumeLayout(False)
-        Me.GroupBox1.PerformLayout()
         Me.Panel1.ResumeLayout(False)
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        FlowLayoutPanel1.ResumeLayout(False)
+        FlowLayoutPanel1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
     Friend WithEvents LblCaption As Label
-    Friend WithEvents GroupBox1 As GroupBox
-    Friend WithEvents RadioFill00 As RadioButton
-    Friend WithEvents RadioFillF6 As RadioButton
     Friend WithEvents Panel1 As Panel
     Friend WithEvents BtnOK As Button
     Friend WithEvents BtnCancel As Button
     Friend WithEvents RadioFillKeep As RadioButton
+    Friend WithEvents RadioFillF6 As RadioButton
+    Friend WithEvents RadioFill00 As RadioButton
 End Class
