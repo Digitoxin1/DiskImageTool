4a001bd63f3fa11f4a11a57d6583b7d76fa145e4
Digitoxin1 <digitoxin@gmail.com>
2025-12-01 20:33:57 -0500

    Incremented version number

--- DiskImageTool/My Project/AssemblyInfo.vb
+++ DiskImageTool/My Project/AssemblyInfo.vb
@@ -31,4 +31,4 @@ Imports System.Runtime.InteropServices
 ' <Assembly: AssemblyVersion("1.0.*")>
 
 <Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("2.30.0.2")>
+<Assembly: AssemblyFileVersion("2.30.0.3")>
